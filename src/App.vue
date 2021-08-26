e508aaafda50dc73138a2799a2d1efd0589b9c0f
Reto Ryter <reto@twy.gmbh>
2021-08-26 15:16:41 +0000

    feature(profile): upload profile page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/profile">Profile</router-link>
   </div>
   <router-view />
 </template>
@@ -11,7 +11,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
---
+++ src/views/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <Upload msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Upload from "@/components/Upload.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    Upload,
+  },
+});
+</script>
