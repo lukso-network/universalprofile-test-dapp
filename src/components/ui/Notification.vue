fbd3415612e5524d332c65a7cf511a28d8d6a9ea
Dominik Zborowski <dominik.zborowski@gmail.com>
2021-10-07 19:47:22 +0200

    Notifications

---
+++ src/components/ui/Notification.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="notification" :class="'is-' + notification.type">
+    <button class="delete" @click="hide"></button>
+    {{ notification.message }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "UiNotification",
+  props: {
+    notification: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      isVisible: true,
+    };
+  },
+
+  methods: {
+    hide() {
+      this.$emit("hide");
+    },
+  },
+});
+</script>
