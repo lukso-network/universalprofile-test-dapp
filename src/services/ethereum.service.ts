f860b1db110efd0d93c884096c94a2152cb3c08c
Dominik Zborowski <dominik.zborowski@gmail.com>
2021-10-15 22:58:32 +0200

    Cleanuo

--- src/services/ethereum.service.ts
+++ src/services/ethereum.service.ts
@@ -1,8 +1,8 @@
 import Web3 from "web3";
 import { TransactionReceipt } from "web3-core";
 
-const web3 = new Web3(window.ethereum);
 const { ethereum } = window;
+const web3 = new Web3(ethereum);
 const WEI_UNIT = "ether";
 
 export async function getAccount(): Promise<string> {
