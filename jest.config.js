407a317078636f9478e3823496de04a322b17707
Dominik Zborowski <dominik.zborowski@gmail.com>
2022-02-17 10:34:11 +0100

    fix: add gas prices for Wallet Connect to work (#22)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
+const esModules = ["@lukso/lsp-smart-contracts"].join("|");
+
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   transform: {
     "^.+\\.vue$": "vue-jest",
   },
   setupFilesAfterEnv: ["<rootDir>/tests/setup-env.ts"],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
 };
