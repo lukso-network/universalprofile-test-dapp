89accd2315ee968d4fe8e6737371fc97bf04ddd8
Dominik Zborowski <dominik.zborowski@gmail.com>
2022-04-19 16:48:42 +0200

    fix: bump up default gas price

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -4,6 +4,6 @@ export const NETWORK_URL = "https://rpc.l14.lukso.network";
 export const UP_CONNECTED_ADDRESS = "up:connected-address";
 
 export const DEFAULT_GAS = 5_000_000;
-export const DEFAULT_GAS_PRICE = "1000000000";
+export const DEFAULT_GAS_PRICE = "10000000000";
 
 export const MAGICVALUE = "0x1626ba7e";
