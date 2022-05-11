7c68909247424c027d9e3c57ac6a24d44a1ff8f5
Dominik Zborowski <dominik.zborowski@gmail.com>
2022-05-11 10:10:40 +0200

    feat: missing extension warning

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,24 @@ import SetData from "@/components/endpoints/SetData.vue";
 import Permissions from "@/components/endpoints/Permissions.vue";
 import Assets from "@/components/endpoints/Assets.vue";
 import Sign from "@/components/endpoints/Sign.vue";
+import Notifications from "@/components/shared/Notification.vue";
+import { computed } from "vue";
+
+const hasExtension = computed(() => {
+  return window.ethereum;
+});
 </script>
 
 <template>
   <section class="section">
+    <Notifications
+      v-if="!hasExtension"
+      :notification="{
+        message: 'Please instal Universal Profile browser extension.',
+        type: 'warning',
+      }"
+      class="mb-4"
+    ></Notifications>
     <div class="tile is-ancestor">
       <Accounts />
       <SendLyx />
