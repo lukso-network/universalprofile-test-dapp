028deb971c39d5ffd9aa4cf8aa0b12d6b186064d
Dominik Zborowski <dominik.zborowski@gmail.com>
2022-06-14 09:50:20 +0200

    feat: validations (#32)

--- src/components/endpoints/__tests__/Assets.spec.ts
+++ src/components/endpoints/__tests__/Assets.spec.ts
@@ -1,7 +1,6 @@
 import Assets from "../Assets.vue";
-import { render, fireEvent, waitFor } from "@testing-library/vue";
+import { render, fireEvent, screen } from "@testing-library/vue";
 import { setState } from "@/stores";
-import { Contract } from "web3-eth-contract";
 
 jest.mock("@/compositions/useWeb3", () => ({
   __esModule: true,
@@ -36,29 +35,26 @@ jest.mock("@/compositions/useWeb3", () => ({
 
 test("can create token", async () => {
   setState("isConnected", true);
-  const utils = render(Assets);
+  render(Assets);
 
-  await fireEvent.click(utils.getByTestId("create"));
-
-  await waitFor(() => {
-    expect(utils.getByTestId("notification").innerHTML).toContain(
-      "Token created"
-    );
-    expect(utils.getByTestId("token-address").innerHTML).toContain(
-      "0x7367C96553Ed4C44E6962A38d8a0b5f4BE9F6298"
-    );
-  });
+  await fireEvent.click(screen.getByTestId("create"));
+  expect(screen.getByTestId("notification")).toHaveTextContent("Token created");
+  expect(screen.getByTestId("token-address")).toHaveTextContent(
+    "0x7367C96553Ed4C44E6962A38d8a0b5f4BE9F6298"
+  );
 });
 
 test("can mint token", async () => {
-  const utils = render(Assets);
+  render(Assets);
 
-  await fireEvent.click(utils.getByTestId("create"));
-  await fireEvent.click(utils.getByTestId("mint"));
+  await fireEvent.click(screen.getByTestId("create"));
+  await fireEvent.update(
+    screen.getByTestId("mint-address"),
+    "0x7367C96553Ed4C44E6962A38d8a0b5f4BE9F6298"
+  );
+  await fireEvent.click(screen.getByTestId("mint"));
 
-  await waitFor(() => {
-    expect(utils.getByTestId("notification").innerHTML).toContain(
-      "Token minted"
-    );
-  });
+  expect(await screen.findByTestId("notification")).toHaveTextContent(
+    "Token minted"
+  );
 });
