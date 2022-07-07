8645cb963f8ea55786334eef6abe00b1bff43886
Andrew Ogaga <andrew.ogaga@seeq.com>
2022-07-07 17:54:24 +0100

    adds mock for useLspFactory

--- src/compositions/__tests__/useLspFactory.spec.ts
+++ src/compositions/__tests__/useLspFactory.spec.ts
@@ -1,22 +1,46 @@
 import { useLspFactory } from "@/compositions/useLspFactory";
 
-test("can produce LSP Factory", async () => {
-  const lspFactory = await useLspFactory();
+jest.mock("@lukso/lsp-factory.js", () => ({
+  LSPFactory: jest.fn().mockImplementation(() => ({
+    ProxyDeployer: {
+      deployBaseContracts: () => ({
+        universalProfile: {
+          address: "0x1",
+        },
+        universalReceiverDelegate: {
+          address: "0x1",
+        },
+      }),
+    },
+    LSP3UniversalProfile: {
+      deploy: () => "success",
+    },
+  })),
+}));
+jest.mock("@/services/provider.service", () => ({
+  getSigner: async () => ({
+    provider: {
+      getNetwork: jest.fn().mockImplementation(async () => ({
+        chainId: 22,
+      })),
+    },
+  }),
+}));
+
+describe("can produce LSP Factory", () => {
+  let lspFactory: any;
+  beforeAll(async () => {
+    lspFactory = await useLspFactory();
+  });
 
   it("can deploy base contracts", async () => {
     const deployedBaseContract =
       await lspFactory.ProxyDeployer.deployBaseContracts();
-    expect(deployedBaseContract.universalProfile.address).toBe(
-      expect.stringContaining("0x")
-    );
-    expect(deployedBaseContract.universalReceiverDelegate.address).toBe(
-      expect.stringContaining("0x")
-    );
+    expect(deployedBaseContract.universalProfile.address).toBe("0x1");
+    expect(deployedBaseContract.universalReceiverDelegate.address).toBe("0x1");
   });
 
   it("can deploy universal profile", async () => {
-    expect(lspFactory.LSP3UniversalProfile).toBe(
-      expect.objectContaining({ deploy: expect.anything() })
-    );
+    expect(lspFactory.LSP3UniversalProfile.deploy()).toBe("success");
   });
 });
--- src/compositions/useLspFactory.ts
+++ src/compositions/useLspFactory.ts
@@ -1,13 +1,12 @@
 import { getSigner } from "@/services/provider.service";
 import { LSPFactory } from "@lukso/lsp-factory.js";
-import useWeb3 from "./useWeb3";
 
 export async function useLspFactory(): Promise<LSPFactory> {
   const { provider } = await getSigner();
-  const rpcProvider = "https://rpc.l16.lukso.network"; // RPC provider url
   const { chainId } = await provider.getNetwork();
-  return new LSPFactory(rpcProvider, {
-    deployKey: "",
+  return new LSPFactory(provider, {
+    deployKey:
+      "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
     chainId,
   });
 }
