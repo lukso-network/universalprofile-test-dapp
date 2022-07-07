9cd52411b16e40335bb896f2c3cbf5f1da2c12a6
Dominik Zborowski <dominik.zborowski@gmail.com>
2022-07-07 10:57:56 +0200

    Cleanup old send lyx

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -56,9 +56,6 @@ const { toggle } = useDropdown();
             <router-link class="navbar-item" to="/profiles/deploy"
               >Deploy</router-link
             >
-            <router-link class="navbar-item" to="/profiles/send-lyx"
-              >Send LYX</router-link
-            >
           </div>
         </div>
 
