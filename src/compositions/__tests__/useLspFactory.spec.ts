c21f388d03b75048a1446e45c58accf55890a1d0
Andrew Ogaga <andrew.ogaga@seeq.com>
2022-07-08 17:11:15 +0100

    improves composable for useLspFactory and its test

--- src/compositions/__tests__/useLspFactory.spec.ts
+++ src/compositions/__tests__/useLspFactory.spec.ts
@@ -1,30 +1,25 @@
 import { useLspFactory } from "@/compositions/useLspFactory";
 import { NETWORK_URL, PRIVATE_KEY } from "@/helpers/config";
-import { getState } from "@/stores";
+import { setState } from "@/stores";
 import { LSPFactory } from "@lukso/lsp-factory.js";
 
 jest.mock("@lukso/lsp-factory.js", () => ({
   LSPFactory: jest.fn(),
 }));
-jest.mock("@/stores", () => ({
-  getState: jest.fn().mockReturnValue(22),
-}));
 
 describe("can produce LSP Factory", () => {
   let lspFactory: any;
   beforeAll(async () => {
+    window.ethereum = {};
     lspFactory = await useLspFactory();
   });
 
-  it("can call correct parameters", async () => {
-    expect(LSPFactory).toBeCalledWith(NETWORK_URL, {
-      deployKey: PRIVATE_KEY,
-      chainId: 22,
-    });
+  it("should be called with window.ethereum", async () => {
+    expect(LSPFactory).toBeCalledWith({});
   });
 
   it("should return null for empty chain id", async () => {
-    (getState as jest.Mock).mockReturnValue(undefined);
+    window.ethereum = undefined;
     lspFactory = await useLspFactory();
     expect(lspFactory).toBe(null);
   });
--- src/compositions/useLspFactory.ts
+++ src/compositions/useLspFactory.ts
@@ -1,14 +1,99 @@
-import { NETWORK_URL, PRIVATE_KEY } from "@/helpers/config";
-import { getState } from "@/stores";
-import { LSPFactory } from "@lukso/lsp-factory.js";
+import { ProfileDataForEncoding } from "@lukso/lsp-factory.js/build/main/src/lib/interfaces/lsp3-profile";
+import {
+  DeployedUniversalProfileContracts,
+  LSPFactory,
+} from "@lukso/lsp-factory.js";
+import {
+  ContractDeploymentOptions,
+  ProfileDataBeforeUpload,
+  ProfileDeploymentOptions,
+} from "@lukso/lsp-factory.js-alpha";
+import {
+  DeployedLSP7DigitalAsset,
+  DeployedLSP8IdentifiableDigitalAsset,
+  DigitalAssetDeploymentOptions,
+  LSP7ContractDeploymentOptions,
+  LSP7DigitalAssetDeploymentOptions,
+  LSP8ContractDeploymentOptions,
+} from "@lukso/lsp-factory.js/build/main/src/lib/interfaces/digital-asset-deployment";
+import { UploadOptions } from "@lukso/lsp-factory.js/build/main/src/lib/interfaces/profile-upload-options";
 
-export async function useLspFactory(): Promise<LSPFactory | null> {
-  const chainId = getState("chainId");
-  if (chainId && NETWORK_URL) {
-    return new LSPFactory(NETWORK_URL, {
-      deployKey: PRIVATE_KEY,
-      chainId,
-    });
+let lspFactory: LSPFactory;
+
+const getFactory = (): LSPFactory => {
+  return lspFactory;
+};
+
+const deployUniversalProfile = async (
+  profileDeploymentOptions: ProfileDeploymentOptions,
+  contractDeploymentOptions?: ContractDeploymentOptions | undefined
+): Promise<DeployedUniversalProfileContracts> => {
+  return await lspFactory.UniversalProfile.deploy(
+    profileDeploymentOptions,
+    contractDeploymentOptions
+  );
+};
+
+const uploadUniversalProfileMetaData = async (
+  profileData: ProfileDataBeforeUpload,
+  uploadOptions?: UploadOptions
+): Promise<ProfileDataForEncoding> => {
+  return await lspFactory.UniversalProfile.uploadProfileData(
+    profileData,
+    uploadOptions
+  );
+};
+
+const deployLsp7DigitalAsset = async (
+  digitalAssetDeploymentOptions: LSP7DigitalAssetDeploymentOptions,
+  contractDeploymentOptions?: LSP7ContractDeploymentOptions
+): Promise<DeployedLSP7DigitalAsset> => {
+  return await lspFactory.LSP7DigitalAsset.deploy(
+    digitalAssetDeploymentOptions,
+    contractDeploymentOptions
+  );
+};
+
+const deployLSP8IdentifiableDigitalAsset = async (
+  digitalAssetDeploymentOptions: DigitalAssetDeploymentOptions,
+  contractDeploymentOptions?: LSP8ContractDeploymentOptions
+): Promise<DeployedLSP8IdentifiableDigitalAsset> => {
+  return await lspFactory.LSP8IdentifiableDigitalAsset.deploy(
+    digitalAssetDeploymentOptions,
+    contractDeploymentOptions
+  );
+};
+
+export function useLspFactory(): {
+  deployLSP8IdentifiableDigitalAsset: (
+    digitalAssetDeploymentOptions: DigitalAssetDeploymentOptions,
+    contractDeploymentOptions?: LSP8ContractDeploymentOptions
+  ) => Promise<DeployedLSP8IdentifiableDigitalAsset>;
+  deployLsp7DigitalAsset: (
+    digitalAssetDeploymentOptions: LSP7DigitalAssetDeploymentOptions,
+    contractDeploymentOptions?: LSP7ContractDeploymentOptions
+  ) => Promise<DeployedLSP7DigitalAsset>;
+  uploadUniversalProfileMetaData: (
+    profileData: ProfileDataBeforeUpload,
+    uploadOptions?: UploadOptions
+  ) => Promise<ProfileDataForEncoding>;
+  deployUniversalProfile: (
+    profileDeploymentOptions: ProfileDeploymentOptions,
+    contractDeploymentOptions?: ContractDeploymentOptions | undefined
+  ) => Promise<DeployedUniversalProfileContracts>;
+  getFactory: () => LSPFactory;
+} | null {
+  const hasExtension = !!window.ethereum;
+  console.log(window.ethereum);
+  if (hasExtension) {
+    lspFactory = new LSPFactory(window.ethereum);
+    return {
+      deployLSP8IdentifiableDigitalAsset,
+      deployLsp7DigitalAsset,
+      uploadUniversalProfileMetaData,
+      deployUniversalProfile,
+      getFactory,
+    };
   }
   return null;
 }
