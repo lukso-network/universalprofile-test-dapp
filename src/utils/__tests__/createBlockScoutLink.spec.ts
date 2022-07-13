d8ea8e421252f0088628a749cc782ff9e8247541
Andrew Ogaga <andrew.ogaga@seeq.com>
2022-07-13 08:26:36 +0100

    changes from dynamic expects

--- src/utils/__tests__/createBlockScoutLink.spec.ts
+++ src/utils/__tests__/createBlockScoutLink.spec.ts
@@ -2,7 +2,8 @@ import { DEFAULT_NETWORK_CONFIG } from "@/helpers/config";
 import { createBlockScoutLink } from "@/utils/createBlockScoutLink";
 
 test("createBlockScoutLink(): returns correct blockscout link", () => {
-  expect(createBlockScoutLink("ABC")).toBe(
-    `${DEFAULT_NETWORK_CONFIG.blockscout.url}/ABC/internal-transactions`
+  const link = createBlockScoutLink("ABC");
+  expect(link).toBe(
+    `https://explorer.execution.l16.lukso.network/tx/ABC/internal-transactions`
   );
 });
