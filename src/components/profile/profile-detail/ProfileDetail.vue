7d7a91328120de5e7af3515e5aa6cc2bd81d89ae
stizzle123 <arifayanidowu2@yahoo.com>
2022-07-18 12:48:13 +0100

    Removed old folders

--- src/components/profile/profile-detail/ProfileDetail.vue
+++
@@ -1,3 +0,0 @@
-<script src="./profile-detail.component.ts" lang="ts"></script>
-
-<template src="./profile-detail.component.html"></template>
--- src/components/profile/profile-upload/ProfileUpload.vue
+++
@@ -1,3 +0,0 @@
-<script src="./profile-upload.component.ts" lang="ts"></script>
-<template src="./profile-upload.component.html"></template>
-<style src="./profile-upload.component.scss" lang="scss"></style>
