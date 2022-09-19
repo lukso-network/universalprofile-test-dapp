40dbbc5a3ac5eff2195127205359070a7060f83b
Hugo Masclet <hugo@lukso.io>
2022-09-19 15:49:48 +0200

    Improve the UI of the ImportAccount card

--- src/components/endpoints/ImportProfile.vue
+++ src/components/endpoints/ImportProfile.vue
@@ -12,7 +12,6 @@ const { isAddress } = useWeb3()
 
 const universalProfileAddress = ref('')
 const controllerAddress = ref('')
-const showPermissions = ref(false)
 
 const onImportProfile = async () => {
   if (
@@ -30,15 +29,10 @@ const onImportProfile = async () => {
       method: 'up_import',
       params: [universalProfileAddress.value],
     })
+    console.log('im here1')
     if (newControllerAddress) {
       controllerAddress.value = newControllerAddress
-      showPermissions.value = true
-      setNotification(
-        `Profile imported successfully.<br/>
-        The dApp should now create a setData request to grant permission to the new controller address: ${newControllerAddress}.<br/>Key: 0x4b80742de2bf82acb3630000${newControllerAddress.slice(
-          2
-        )}<br/>value (full access): 0x00000000000000000000000000000000000000000000000000000000000003ff`
-      )
+      console.log('im here')
     }
   } catch (error) {
     setNotification((error as unknown as Error).message, 'danger')
@@ -48,7 +42,7 @@ const onImportProfile = async () => {
 
 <template>
   <div class="tile is-4 is-parent">
-    <div class="tile is-child box">
+    <div class="tile is-child box" :style="{ overflow: 'auto' }">
       <p class="is-size-5 has-text-weight-bold mb-4">Import Profile</p>
 
       <div class="field">
@@ -64,6 +58,25 @@ const onImportProfile = async () => {
         </div>
       </div>
 
+      <div v-if="controllerAddress" class="notification is-info is-light mt-5">
+        <p>Profile imported successfully.</p>
+        <p>
+          The dApp should now create a setData request to grant permission to
+          the new controller address: {{ controllerAddress }}.<br />
+          Key: 0x4b80742de2bf82acb3630000{{ controllerAddress.slice(2) }}<br />
+          value (full access):
+          0x00000000000000000000000000000000000000000000000000000000000003ff`
+        </p>
+      </div>
+
+      <div v-if="hasNotification" class="field">
+        <Notifications
+          :notification="notification"
+          class="mt-4"
+          @hide="clearNotification"
+        />
+      </div>
+
       <div class="field">
         <button
           :class="`button is-primary is-rounded mt-4`"
@@ -72,25 +85,6 @@ const onImportProfile = async () => {
         >
           Import Profile
         </button>
-      </div>
-
-      <div v-if="controllerAddress" class="field">
-        <button
-          :class="`button is-primary is-rounded mt-4`"
-          data-testid="import-profile"
-          @click.stop="onImportProfile"
-        >
-          Request access to: {{ controllerAddress }}
-        </button>
-      </div>
-
-      <div class="field">
-        <Notifications
-          v-if="hasNotification"
-          :notification="notification"
-          class="mt-4"
-          @hide="clearNotification"
-        />
       </div>
 
       <div>
