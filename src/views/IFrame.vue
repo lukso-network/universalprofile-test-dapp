fe495bf3fae53f1849d6674d42c8cf143879e2c1
Andreas Richter <708186+richtera@users.noreply.github.com>
2023-03-22 09:05:09 -0400

    fix: IFrame should also use public url

--- src/views/IFrame.vue
+++ src/views/IFrame.vue
@@ -1,8 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const base = ref<string>(import.meta.env.BASE_URL || '/')
+</script>
+
 <template>
   <iframe
     width="1080"
     height="800"
-    src="/?hideNav=true"
+    :src="base + '?hideNav=true'"
     sandbox="allow-same-origin allow-scripts"
   ></iframe>
 </template>
