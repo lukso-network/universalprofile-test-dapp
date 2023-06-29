69c2cf470e414c21439d639d26865f5616bec5c8
Andy Ogaga <44970013+andyogaga@users.noreply.github.com>
2023-06-29 11:34:14 +0100

    fixes rpc request returns

--- src/helpers/customRequest.ts
+++ src/helpers/customRequest.ts
@@ -6,9 +6,8 @@ const sendRequest = async (request: any): Promise<any> => {
   const wcv2Provider = getWCV2Provider()
   if (wcv2Provider && wcv2Provider.connected) {
     return await sendCustomWCV2Request(request)
-  } else {
-    return await (window.ethereum as any)?.request(request)
   }
+  return await (window.ethereum as any)?.request(request)
 }
 
 export { sendRequest }
