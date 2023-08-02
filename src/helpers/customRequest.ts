9738ac0faf56d96eb1e5d8d6b17a19b1a79bc2c1
Jenea Vranceanu <jeneavranceanu@gmail.com>
2023-08-02 10:34:53 +0300

    fix: sendRequest function now relies on localStorage.getItem(MEANS_OF_CONNECTION) instead of isDesktop() function

--- src/helpers/customRequest.ts
+++ src/helpers/customRequest.ts
@@ -1,18 +1,19 @@
 import useWalletConnectV2 from '@/compositions/useWalletConnectV2'
-import { isDesktop } from '@/utils/isDesktop'
+import { MEANS_OF_CONNECTION, WALLET_CONNECT } from '@/helpers/config'
 
 const { getWCV2Provider, sendCustomWCV2Request } = useWalletConnectV2()
 
 const sendRequest = async (request: any): Promise<any> => {
-  if (isDesktop()) {
-    return await (window.ethereum as any)?.request(request)
-  } else {
+  const channel = localStorage.getItem(MEANS_OF_CONNECTION)
+  const isWalletConnectUsed = channel === WALLET_CONNECT
+  if (isWalletConnectUsed) {
     const wcv2Provider = getWCV2Provider()
-    if (wcv2Provider && wcv2Provider.connected) {
+    if (wcv2Provider?.connected) {
       return await sendCustomWCV2Request(request)
     }
+  } else {
+    return await (window.ethereum as any)?.request(request)
   }
-  return await (window.ethereum as any)?.request(request)
 }
 
 export { sendRequest }
