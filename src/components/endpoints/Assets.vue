9361994338078a124596859c85858748e94c0b32
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-09-16 15:29:18 +0200

    Add creator to LSP7

--- src/components/endpoints/Assets.vue
+++ src/components/endpoints/Assets.vue
@@ -79,6 +79,7 @@ const create = async () => {
           controllerAddress: erc725AccountAddress,
           name: token.value.name,
           symbol: token.value.symbol,
+          creators: [erc725AccountAddress],
           digitalAssetMetadata: {
             LSP4Metadata: {
               ...lsp4Metadata.value,
