97850be51ec756e6b5ad66e751e23309a351dd19
Jenea Vranceanu <jeneavranceanu@gmail.com>
2024-04-11 11:52:55 +0300

    fix: fixed Permissions.vue tests

--- src/components/endpoints/__tests__/Permissions.spec.ts
+++ src/components/endpoints/__tests__/Permissions.spec.ts
@@ -31,7 +31,7 @@ beforeEach(() => {
 })
 
 test('can update permissions for given address', async () => {
-  setState('address', '0x517216362D594516c6f96Ee34b2c502d65B847E4')
+  setState('address', '0x02f02b27eDFcBBDE762Ff2a7FC20a4Aebd495214')
 
   mockSend.mockImplementation(() => ({
     on: () => ({
@@ -44,17 +44,23 @@ test('can update permissions for given address', async () => {
   await fireEvent.click(screen.getByTestId('CHANGEOWNER'))
   await fireEvent.click(screen.getByTestId('setPermissions'))
 
-  expect(screen.getByTestId('notification')).toHaveTextContent(
+  expect(await screen.findByTestId('notification')).toHaveTextContent(
     'Permissions set'
   )
   expect(mockSend).toBeCalledWith(
-    ['0x4b80742de2bf82acb3630000af3bf2ffb025098b79caddfbdd113b3681817744'],
-    ['0x0000000000000000000000000000000000000000000000000000000000000001']
+    ['0x4b80742de2bf82acb3630000af3bf2ffb025098b79caddfbdd113b3681817744',
+      '0xdf30dba06db6a30e65354d9a64c609861f089545ca58c6b4dbe31a5f338cb0e3',
+      '0xdf30dba06db6a30e65354d9a64c6098600000000000000000000000000000007'
+    ],
+    ['0x0000000000000000000000000000000000000000000000000000000000000001',
+      '0x00000000000000000000000000000008',
+      '0xaf3bf2ffb025098b79caddfbdd113b3681817744'
+    ]
   )
 })
 
 test('can see set permission error from send function', async () => {
-  setState('address', '0x517216362D594516c6f96Ee34b2c502d65B847E4')
+  setState('address', '0x02f02b27eDFcBBDE762Ff2a7FC20a4Aebd495214')
 
   mockSend.mockImplementation(() =>
     jest.fn().mockImplementation(() => {
@@ -66,5 +72,5 @@ test('can see set permission error from send function', async () => {
 
   await fireEvent.click(screen.getByTestId('setPermissions'))
 
-  expect(screen.getByTestId('notification')).toHaveTextContent('Send error')
+  expect(await screen.findByTestId('notification')).toHaveTextContent('Send error')
 })
