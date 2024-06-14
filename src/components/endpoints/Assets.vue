579084cd92dfc2d3865faa0783e2e7cc55edea95
Jenea Vranceanu <jeneavranceanu@gmail.com>
2024-06-14 11:43:38 +0300

    fix: lint fix?

--- src/components/endpoints/Assets.vue
+++ src/components/endpoints/Assets.vue
@@ -119,9 +119,8 @@ const create = async () => {
           tokenIdFormat: tokenIdType.value,
         }
         console.log(digitalAssetData)
-        deployedAsset = await deployLSP8IdentifiableDigitalAsset(
-          digitalAssetData
-        )
+        deployedAsset = 
+          await deployLSP8IdentifiableDigitalAsset(digitalAssetData)
         console.log(
           'Deployed asset',
           deployedAsset.LSP8IdentifiableDigitalAsset
