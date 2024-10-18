4ee45c210b4936d523375ce8a1b0a88d7e8f9cf2
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-10-17 21:08:19 -0400

    fix: More work

--- src/components/grid/GridPanelDialog.vue
+++ src/components/grid/GridPanelDialog.vue
@@ -1,19 +1,37 @@
 <script setup lang="ts">
 import { TokenInfo } from '@/helpers/tokenUtils'
+import { getState } from '@/stores'
 import { getUPProviderChannel } from '@lukso/embedded-provider'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 type Props = {
   channel: ReturnType<typeof getUPProviderChannel>
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
 const pageAddress = ref<string>()
+const enabled = ref<boolean>(false)
 function handlePageAddress(info: TokenInfo) {
   if (info.address) {
     pageAddress.value = info.address
   }
 }
+watch(
+  () =>
+    [enabled.value, props.channel] as [
+      boolean,
+      ReturnType<typeof getUPProviderChannel>,
+    ],
+  ([value, channel]: [boolean, ReturnType<typeof getUPProviderChannel>]) => {
+    if (channel) {
+      channel.allowAccounts(
+        [getState('address'), (pageAddress.value || '') as '' | `0x${string}`],
+        getState('chainId')
+      )
+      channel.enabled = value
+    }
+  }
+)
 </script>
 
 <template>
@@ -35,6 +53,17 @@ function handlePageAddress(info: TokenInfo) {
             data-testid="transfer-address"
           />
         </div>
+      </div>
+      <div class="field mt-4">
+        <label class="checkbox">
+          <input
+            v-model="enabled"
+            type="checkbox"
+            :value="enabled"
+            data-testid="isRecovery"
+          />
+          Recover the profile into your extension
+        </label>
       </div>
     </div>
   </div>
